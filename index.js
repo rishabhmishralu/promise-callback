d994bce8021624cf03f8edd76a3eef807bee0304
= <=>
2022-12-23 17:14:45 +0530

    add

---
+++ index.js
@@ -0,0 +1,104 @@
+// function sum(a,b,callback){  
+//      console.log(a+b);
+//      callback()
+// }
+// function name(name){
+//     console.log("name");
+// }
+
+// sum(2,3,name)
+
+// setTimeout(() => {
+//     // for(let i=1;i<=7;i++)
+//     console.log("1");
+    
+// },1000);
+
+
+
+// const user=fetch()
+// console.log(user);
+
+
+
+
+// let complete=true
+
+// let prom=new Promise(function(resolve,reject){
+// if(complete){
+//    resolve("im pass");
+// }else {
+// reject("im failed")
+// }
+
+// })
+// console.log(prom);
+
+
+// let complete=false
+
+// let prom=new Promise(function(resolve,reject){
+// if(complete){
+//    resolve();
+// }else {
+// reject()
+// }
+
+// })
+// prom.then(function(){
+//     console.log("yes");
+// }).catch(function(error){
+//     console.log("error");
+// })
+
+
+
+
+//callback function
+
+// function add(a,b,callback){
+//     callback()
+//     console.log(a+b);
+    
+// }
+// function multiply(num1,num2){
+//     console.log(num1*num2)
+// }
+
+// add(3,4,multiply)
+
+
+//  async function sum(){
+//    return("hello");
+
+// }
+//  sum().then((result)=>{
+//      console.log(result);
+//  })
+
+
+
+
+
+ 
+// async function name(){
+//     console.log("rishabh");
+//    await console.log("mishra");
+//     console.log("aryan");
+// }
+// name()
+//     console.log("hello");
+
+// 5
+ function hello(){
+    console.log("heloo...");
+ }
+ function sum(num1,num2,callback){
+ console.log(num1+num2);
+ hello()
+ }
+
+sum(3,4,hello)
+
+
+
