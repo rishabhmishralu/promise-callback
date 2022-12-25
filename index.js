af4b06ff0f10545261a07276b5ef35fb1592d201
= <=>
2022-12-25 12:44:53 +0530

    chages

--- index.js
+++ index.js
@@ -90,15 +90,105 @@
 //     console.log("hello");
 
 // 5
- function hello(){
-    console.log("heloo...");
- }
- function sum(num1,num2,callback){
- console.log(num1+num2);
- hello()
- }
+//  function hello(){
+//     console.log("heloo...");
+//  }
+//  function sum(num1,num2,callback){
+//  console.log(num1+num2);
+//  hello()
+//  }
 
-sum(3,4,hello)
+// sum(3,4,hello)
+
+// function calltime(){
+//     setTimeout(()=>{
+//         console.log("1");
+//         setTimeout(()=>{
+//             console.log("2");
+//             setTimeout(()=>{
+//                 console.log("3");
+//                 setTimeout(()=>{
+//                     console.log("4");
+//                     setTimeout(()=>{
+//                         console.log("5");
+//                         setTimeout(()=>{
+//                             console.log("6");
+//                              setTimeout(()=>{
+//                                 console.log("7");
+//                               },7000)
+//                               },6000)
+//                              },5000)
+//                            },4000)
+//                             },3000)
+//                        },2000)
+//                    },1000)
+
+// }
+// calltime()
+
+
+// callback hell
+// function print(){
+//     console.log("hello");
+// }
+// function add(r){
+//     console.log("add two number");
+//     print()
+// }
+// function sum(n1,n2,h){
+//     console.log(n1+n2);
+//     add()
+// }
+// sum(1,2,add)
 
 
 
+
+//3
+// function yes(yes) {
+// let prom=new Promise(function(resolve,reject){
+//     if(true){
+//         resolve("Promise Resolved")
+//     }
+//     else{
+//         reject('Promise Rejected')
+//     }
+//     })
+//      let onyes=(result)=>{
+//         console.log(result);
+//     }
+//     let galat=(error)=>{
+//         console.log(error);
+//     }
+//     prom.then(onyes);
+//     prom.catch(galat)     
+// })
+// yes(yes)
+
+
+let  p1=new Promise(function(resolve,reject){
+   setTimeout(()=>{
+    console.log("the first promise resolved");
+    resolve(10);
+   },1*1000)
+})
+
+
+let  p2=new Promise(function(resolve,reject){
+    setTimeout(()=>{
+     console.log("the second promise resolved");
+     resolve(20);
+    },2*1000)
+ })
+ let  p3=new Promise(function(resolve,reject){
+    setTimeout(()=>{
+     console.log("the third promise resolved");
+     resolve(30);
+    },3*1000)
+ })
+
+ Promise.all([p1,p2,p3]).then((result)=>{
+    console.log(`result:${result}`);
+ }).catch((error)=>{
+    console.log(`result:${error}`)})
+ 
